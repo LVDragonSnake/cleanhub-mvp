6246c30837864cbd8a8796b3a2bcc799a38f4dcc
LVDragonSnake <163784876+LVDragonSnake@users.noreply.github.com>
2026-02-09 15:35:19 +0100

    Update WorkerHeader.tsx

--- app/components/WorkerHeader.tsx
+++ app/components/WorkerHeader.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
-import { progressFromXp } from "../lib/gamification";
+import { progressFromXp } from "../../lib/gamification";
 
 type ProfileLite = {
   first_name: string | null;
